dc638a8f31880d18f129401f36cf8775ab18cc76
LucasLaraITX <lucasjlg@ext.inditex.com>
2026-01-29 20:31:10 +0100

    Refactor job listing layout and improve button functionality for better user experience

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const jobResults = document.querySelector('.job-listings');
 
 jobResults?.addEventListener('click', (event) => {
   const element = event.target
-  if(element.classList.contains('job-listing-button'))  {
+  if (element.classList.contains('job-listing-button')) {
     element.classList.add('is-applied')
     element.textContent = '¡Aplicado!'
     element.disabled = true;
