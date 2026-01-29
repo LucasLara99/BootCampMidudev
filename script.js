f33057f254ffa614437adcbf90e47330b74f659f
LucasLaraITX <lucasjlg@ext.inditex.com>
2026-01-29 19:46:04 +0100

    Add optional chaining to jobResults event listener for safer DOM access

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const jobResults = document.querySelector('.job-listings');
 
-jobResults.addEventListener('click', (event) => {
+jobResults?.addEventListener('click', (event) => {
   const element = event.target
   if(element.classList.contains('job-listing-button'))  {
     element.classList.add('is-applied')
