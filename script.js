4ffa349d3bcca4165b13db2e07031be30a21d8e0
LucasLaraITX <lucasjlg@ext.inditex.com>
2026-02-10 10:08:04 +0100

    Refactor job filtering functionality to dynamically show/hide job listings based on selected technology

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ jobResults?.addEventListener('click', (event) => {
     element.disabled = true;
   }
 })
+
+const techFilter = document.querySelector('#technology-filter');
+const jobCards = document.querySelectorAll('.job-listing-card');
+techFilter.addEventListener('change', () => {
+  jobCards.forEach((jobOffer) => {
+    jobOffer.classList.remove('hidden')
+    const description = jobOffer.querySelector('.job-description').textContent
+    if (!description.includes(techFilter.value)) {
+      jobOffer.classList.add('hidden')
+    }
+  })
+})
+
+
