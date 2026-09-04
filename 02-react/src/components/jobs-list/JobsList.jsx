8794bb71e9b42011dbbfc8807d1d1294fb0c0b9e
LucasLara99 <lucaslara99@gmail.com>
2026-09-04 15:02:18 +0200

    feat: add empty state handling to JobsList component for better user experience

--- 02-react/src/components/jobs-list/JobsList.jsx
+++ 02-react/src/components/jobs-list/JobsList.jsx
@@ -2,15 +2,20 @@ import './JobsList.css'
 import jobsData from '../../data.json'
 import { JobCard } from '../jobcard/JobCard'
 
-
 export function JobsList() {
+   const isJobsDataEmpty = jobsData.length === 0
+
    return (
       <section>
          <h2 className="job-list-title">Resultados de búsqueda</h2>
          <div className="job-listings">
-            {jobsData.map((job) => (
-               <JobCard key={job.id} job={job} />
-            ))}
+            {
+               !isJobsDataEmpty ? (
+                  jobsData.map((job) => (
+                     <JobCard key={job.id} job={job} />
+                  ))
+               ) : (<p className='empty-data-message'>No hay ningún resultado</p>)
+            }
          </div>
       </section>
    )
