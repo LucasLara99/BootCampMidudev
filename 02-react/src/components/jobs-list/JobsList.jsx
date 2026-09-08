317c06e0c046992241bbe45f180eb03f6c5c6df3
LucasLara99 <lucaslara99@gmail.com>
2026-09-08 19:14:29 +0200

    feat: refactor JobsList component to use CSS modules and improve styling; remove unused CSS files

--- 02-react/src/components/jobs-list/JobsList.jsx
+++ 02-react/src/components/jobs-list/JobsList.jsx
@@ -1,21 +1,21 @@
-import './JobsList.css'
+import styles from './JobsList.module.css'
 import { JobCard } from '../jobcard/JobCard'
 
 export function JobsList({ jobs }) {
    const isJobsDataEmpty = jobs.length === 0
 
    return (
-      <section className='job-list-section'>
-         <h2 className="job-list-title">Resultados de búsqueda</h2>
-         <div className="job-listings">
-            {
-               !isJobsDataEmpty ? (
-                  jobs.map((job) => (
+      <section className={styles.jobsListSection}>
+         <h2 className={styles.jobsListTitle}>Resultados de búsqueda</h2>
+         {
+            !isJobsDataEmpty ? (
+               <div className={styles.jobsListing}>
+                  {jobs.map((job) => (
                      <JobCard key={job.id} job={job} />
-                  ))
-               ) : (<p className='empty-data-message'>No hay ningún resultado</p>)
-            }
-         </div>
+                  ))}
+               </div>
+            ) : (<i className={styles.noData}>No se han encontrado resultado</i>)
+         }
       </section>
    )
 }
