3efb4bb98f8b9a802c5e4491297eb7093ee83eba
LucasLara99 <lucaslara99@gmail.com>
2026-09-21 20:21:02 +0200

    feat: update useSearch hook to improve job fetching with enhanced error handling and pagination, increase results per page to 5

--- 02-react/src/hooks/useSearch.jsx
+++ 02-react/src/hooks/useSearch.jsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react"
 
-const RESULTS_PER_PAGE = 3
+const RESULTS_PER_PAGE = 5
 
 export function useSearch() {
    const [currentPage, setCurrentPage] = useState(1)
    const [textToFilter, setTextToFilter] = useState('')
    const [jobs, setJobs] = useState([])
    const [total, setTotal] = useState(0)
+   const [error, setError] = useState(null)
    const [isLoading, setIsLoading] = useState(false)
    const [filters, setFilters] = useState({
       technology: '',
@@ -32,45 +33,57 @@ export function useSearch() {
       async function fetchJobs() {
          try {
             setIsLoading(true)
+            setError(null)
+            const params = new URLSearchParams()
 
-            //Simulacion
-            await new Promise((resolve) => setTimeout(resolve, 5000))
+            if (textToFilter) {
+               params.append('text', textToFilter)
+            }
 
-            const response = await fetch('https://jscamp-api.vercel.app/api/jobs')
+            if (filters.technology) {
+               params.append('technology', filters.technology)
+            }
+
+            if (filters.location) {
+               params.append('type', filters.location)
+            }
+
+            if (filters.experience) {
+               params.append('level', filters.experience)
+            }
+
+            const offset = (RESULTS_PER_PAGE * (currentPage - 1))
+            params.append('limit', RESULTS_PER_PAGE)
+            params.append('offset', offset)
+            const queryParams = params.toString()
+
+            if (!navigator.onLine) throw new Error('NO_INTERNET')
+
+            const response = await fetch(`https://jscamp-api.vercel.app/api/jobs?${queryParams}`)
+
+            if (!response.ok) {
+               if (response.status === 404) throw new Error('404')
+               if (response.status >= 500) throw new Error('500')
+               throw new Error('HTTP_ERROR')
+            }
+
             const json = await response.json()
 
             setJobs(json.data)
             setTotal(json.total)
+
          } catch (error) {
             console.error('Error obteniendo los datos: ', error)
+            setError(error)
          } finally {
             setIsLoading(false)
          }
       }
 
       fetchJobs()
-   }, [])
+   }, [currentPage, textToFilter, filters])
 
-   // const jobsWithFilters = jobsData.filter((job) => {
-   //    return (
-   //       (filters.technology === '' || filters.technology.toLowerCase() === job.data.technology.toLowerCase()) &&
-   //       (filters.location === '' || filters.location.toLowerCase() === job.ubicacion.toLowerCase()) &&
-   //       (filters.experience === '' || filters.experience.toLowerCase() === job.data.nivel.toLowerCase())
-   //    )
-   // })
-
-   // const jobsWithTextFilter = textToFilter === ''
-   //    ? jobsWithFilters
-   //    : jobsWithFilters.filter(job => {
-   //       return job.titulo.toLowerCase().includes(textToFilter.toLowerCase())
-   //    })
-
-   // const pagedResults = jobsWithTextFilter.slice(
-   //    (currentPage - 1) * RESULTS_PER_PAGE,
-   //    RESULTS_PER_PAGE * currentPage
-   // )
-
-   const totalPages = Math.ceil(jobs.length / RESULTS_PER_PAGE)
+   const totalPages = Math.ceil(total / RESULTS_PER_PAGE)
 
    return {
       handleTextFilter,
